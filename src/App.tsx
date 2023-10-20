b8e8e745c55f1eead14c7cccf848062826914666
Andy Nguyen <andy.nguyen.goldenowl@gmail.com>
2023-10-20 16:34:09 +0700

    fix: test new commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,7 @@ function App(): JSX.Element {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
