7d421bd5e013e3664a8e5d98b8360aa6e36ee70a
Andy Nguyen <andy.nguyen.goldenowl@gmail.com>
2023-10-20 15:56:44 +0700

    fix: valid commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> test invalid commit.
         </p>
         <a
           className="App-link"
